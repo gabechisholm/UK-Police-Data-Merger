6a81d79dcf421b121d5f23bdc7933f4bc9e9edf4
Gabe Chisholm <94998324+gabechisholm@users.noreply.github.com>
2025-01-16 12:53:09 +0000

    Update PoliceDataMerger.R

--- PoliceDataMerger.R
+++ PoliceDataMerger.R
@@ -1,8 +1,6 @@
 library(dplyr)
 
-# Function to process the zip file and merge CSVs
 process_zip <- function(zip_path, output_csv) {
-  # Create a temporary directory for extraction
   temp_dir <- tempdir()
   unzip(zip_path, exdir = temp_dir)
   folders <- list.dirs(temp_dir, recursive = FALSE)
@@ -31,6 +29,6 @@ process_zip <- function(zip_path, output_csv) {
   message("Combined CSV saved to: ", output_csv)
 }
 
-zip_file <- "C:\\Users\\RNSMGC\\Downloads\\0f8c5c46d9112b860aadb00e5eeac595c4a6bdf0.zip" # zip file path
-output_file <- "C:\\Users\\RNSMGC\\Downloads\\data_temp\\combined_data.csv"     # desired output CSV file name
+zip_file <- "ZIP-FILE-PATH" # zip file path
+output_file <- "OUTPUT-FILE-PATH\\combined_data.csv"     # desired output CSV file name
 process_zip(zip_file, output_file)
